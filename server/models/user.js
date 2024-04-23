2979b5e73bac88fe13a3e214e4c589def5ce55bb
robbysaputra <robbysaputra300@gmail.com>
2024-04-23 15:51:06 +0700

    server-setup

---
+++ server/models/user.js
@@ -0,0 +1,60 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const { hashPassword } = require('../helpers/bycript');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      allowNull:false,
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "username is required"
+        },
+        notNull: {
+          msg: "username is required"
+        }
+      }
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: {
+        args: true,
+        msg: "Email mus be unique"
+      }
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "password is required"
+        },
+        notNull: {
+          msg: "password is required"
+        }
+      }
+    }, 
+  }, {
+    sequelize,
+    modelName: 'User',
+    hooks: {
+      beforeCreate(user) {
+        user.password = hashPassword(user.password);
+      },
+    },
+  });
+  return User;
+};
---
+++ server/router/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const UserController = require('../Controllers/UserController');
+const router = express.Router();
+
+
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+
+
+
+
+module.exports = router;
