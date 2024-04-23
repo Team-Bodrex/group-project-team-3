4f08e710722b31f79e12e13b2ebad74529e3e299
namegit config --global user.name <bryankenley@yahoo.com>
2024-04-23 23:03:22 +0700

    added socket js

--- client/TicTacToe-Game/src/App.jsx
+++ client/TicTacToe-Game/src/App.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-import { io } from "socket.io-client";
-const socket = io("http://localhost:3000");
+import socket from "./socket";
 
 function App() {
   const [count, setCount] = useState(0);
