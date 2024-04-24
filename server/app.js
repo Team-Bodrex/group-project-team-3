f81651da8a30351e2ae434853520d55892d1ca4a
ValdiF11 <valdifirstianto@gmail.com>
2024-04-24 20:44:54 +0700

    feat: add room for more of 2 player

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -16,9 +16,9 @@ const { Server } = require("socket.io");
 // Start of Socket Settings
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:5173",
-    },
+  cors: {
+    origin: "http://localhost:5173",
+  },
 });
 // End of Socket Settings
 
@@ -30,105 +30,109 @@ const allUsers = {};
 const allRooms = [];
 
 io.on("connection", (socket) => {
-    allUsers[socket.id] = {
-        socket: socket,
-        online: true,
-    };
-    // console.log("New user connected!", socket.id);
-    // console.log("username :", socket.handshake.auth.username);
+  allUsers[socket.id] = {
+    socket: socket,
+    online: true,
+  };
+  // console.log("New user connected!", socket.id);
+  // console.log("username :", socket.handshake.auth.username);
 
-    if (socket.handshake.auth.username) {
-        users.push({
-            id: socket.id,
-            username: socket.handshake.auth.username,
-        });
+  if (socket.handshake.auth.username) {
+    users.push({
+      id: socket.id,
+      username: socket.handshake.auth.username,
+    });
+  }
+
+  console.log(users);
+
+  //socket.emit infoin ke orang yang connect kalau dia dapat message
+  socket.emit("message", "Welcome to the socket server" + socket.id);
+
+  //io.emit info ke semua orang kalo ada user yang connect
+  io.emit("users:online", users);
+
+  socket.on("messages:new", (newMessage) => {
+    io.emit("messages:info", newMessage);
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user.id !== socket.id);
+    io.emit("users:online", users);
+  });
+
+  //Start of Game Sockets
+  socket.on("request_to_play", (data) => {
+    const currentUser = allUsers[socket.id];
+    currentUser.playerName = data.playerName;
+
+    let opponentPlayer;
+
+    // Check room with one player
+    const roomWithOnePlayer = allRooms.find((room) => room.player2 === undefined);
+
+    // If there's a room with only one player, add current user as the opponent player
+    if (roomWithOnePlayer) {
+      roomWithOnePlayer.player2 = currentUser;
+      opponentPlayer = roomWithOnePlayer.player1;
+    } else {
+      // If all rooms are full, create a new room
+      const newRoom = {
+        player1: currentUser,
+        player2: undefined,
+      };
+      allRooms.push(newRoom);
+      opponentPlayer = undefined;
     }
 
-    console.log(users);
+    if (opponentPlayer) {
+      // Start the game
+      currentUser.socket.emit("OpponentFound", {
+        opponentName: opponentPlayer.playerName,
+        playingAs: "circle",
+      });
 
-    //socket.emit infoin ke orang yang connect kalau dia dapat message
-    socket.emit("message", "Welcome to the socket server" + socket.id);
+      opponentPlayer.socket.emit("OpponentFound", {
+        opponentName: currentUser.playerName,
+        playingAs: "cross",
+      });
 
-    //io.emit info ke semua orang kalo ada user yang connect
-    io.emit("users:online", users);
+      currentUser.socket.on("playerMoveFromClient", (data) => {
+        opponentPlayer.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
 
-    socket.on("messages:new", (newMessage) => {
-        io.emit("messages:info", newMessage);
-    });
+      opponentPlayer.socket.on("playerMoveFromClient", (data) => {
+        currentUser.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+    } else {
+      currentUser.socket.emit("OpponentNotFound");
+    }
+  });
 
-    socket.on("disconnect", () => {
-        users = users.filter((user) => user.id !== socket.id);
-        io.emit("users:online", users);
-    });
+  socket.on("disconnect", function () {
+    const currentUser = allUsers[socket.id];
+    currentUser.online = false;
+    currentUser.playing = false;
 
+    for (let index = 0; index < allRooms.length; index++) {
+      const { player1, player2 } = allRooms[index];
 
-    //Start of Game Sockets
-    socket.on("request_to_play", (data) => {
-        const currentUser = allUsers[socket.id];
-        currentUser.playerName = data.playerName;
+      if (player1.socket.id === socket.id) {
+        player2.socket.emit("opponentLeftMatch");
+        break;
+      }
 
-        let opponentPlayer;
-
-        for (const key in allUsers) {
-            const user = allUsers[key];
-            if (user.online && !user.playing && socket.id !== key) {
-                opponentPlayer = user;
-                break;
-            }
-        }
-
-        if (opponentPlayer) {
-            allRooms.push({
-                player1: opponentPlayer,
-                player2: currentUser,
-            });
-
-            currentUser.socket.emit("OpponentFound", {
-                opponentName: opponentPlayer.playerName,
-                playingAs: "circle",
-            });
-
-            opponentPlayer.socket.emit("OpponentFound", {
-                opponentName: currentUser.playerName,
-                playingAs: "cross",
-            });
-
-            currentUser.socket.on("playerMoveFromClient", (data) => {
-                opponentPlayer.socket.emit("playerMoveFromServer", {
-                    ...data,
-                });
-            });
-
-            opponentPlayer.socket.on("playerMoveFromClient", (data) => {
-                currentUser.socket.emit("playerMoveFromServer", {
-                    ...data,
-                });
-            });
-        } else {
-            currentUser.socket.emit("OpponentNotFound");
-        }
-    });
-
-    socket.on("disconnect", function () {
-        const currentUser = allUsers[socket.id];
-        currentUser.online = false;
-        currentUser.playing = false;
-
-        for (let index = 0; index < allRooms.length; index++) {
-            const { player1, player2 } = allRooms[index];
-
-            if (player1.socket.id === socket.id) {
-                player2.socket.emit("opponentLeftMatch");
-                break;
-            }
-
-            if (player2.socket.id === socket.id) {
-                player1.socket.emit("opponentLeftMatch");
-                break;
-            }
-        }
-    });
-    //End of Game Sockets
+      if (player2.socket.id === socket.id) {
+        player1.socket.emit("opponentLeftMatch");
+        break;
+      }
+    }
+  });
+  //End of Game Sockets
 });
 // End of Socket Settings
 
@@ -138,7 +142,7 @@ app.use(cors());
 
 // Testing Site
 app.get("/", (req, res) => {
-    res.send("Hello World! Testing");
+  res.send("Hello World! Testing");
 });
 // Testing Site
 
@@ -150,7 +154,7 @@ app.use(errorHandlers);
 
 // Start of Socket Settings
 httpServer.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
 // End of Socket Settings
 
