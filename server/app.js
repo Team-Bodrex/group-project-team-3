440b1a91399a83b5dfaa36f354d80817696bf1af
ValdiF11 <valdifirstianto@gmail.com>
2024-04-24 13:27:17 +0700

    fix:some part in  server game

--- server/app.js
+++ server/app.js
@@ -26,6 +26,8 @@ const port = 3000;
 
 // Start of Socket Settings
 let users = [];
+const allUsers = {};
+const allRooms = [];
 
 io.on("connection", (socket) => {
   // console.log("New user connected!", socket.id);
@@ -98,6 +100,26 @@ io.on("connection", (socket) => {
       });
     } else {
       currentUser.socket.emit("OpponentNotFound");
+    }
+  });
+
+  socket.on("disconnect", function () {
+    const currentUser = allUsers[socket.id];
+    currentUser.online = false;
+    currentUser.playing = false;
+
+    for (let index = 0; index < allRooms.length; index++) {
+      const { player1, player2 } = allRooms[index];
+
+      if (player1.socket.id === socket.id) {
+        player2.socket.emit("opponentLeftMatch");
+        break;
+      }
+
+      if (player2.socket.id === socket.id) {
+        player1.socket.emit("opponentLeftMatch");
+        break;
+      }
     }
   });
 });
